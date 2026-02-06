2f4aeddf9c17282667f35490a26a44cf8e28d00a
Mr-Taylor-French <jacen.taylor12@gmail.com>
2026-02-05 23:04:39 -0700

    Got most of the core structure down, most functions and wrote my logic of main loop

--- milestone2/main.cpp
+++ milestone2/main.cpp
@@ -2,7 +2,7 @@
 #include <fstream>
 #include <string>
 #include <vector>
-
+#include <filesystem>
 /*
 
 READ = 10 Read a word from the keyboard into a specific location in memory.
@@ -24,13 +24,38 @@ BRANCHNEG = 41 Branch to a specific location in memory if the accumulator is neg
 BRANCHZERO = 42 Branch to a specific location in memory if the accumulator is zero.
 HALT = 43 Pause the program
  */
+
+
+//helper function section
+std::vector<std::string> get_all_txt(){
+	namespace fs = std::filesystem;
+	std::vector<std::string> vec;
+	for (const auto& entry : fs::directory_iterator(fs::current_path())){
+		if (entry.is_regular_file() && entry.path().extension() == ".txt") {//regular file means not a folder, not a .gitignore, nothing weird like that
+			std::string file = entry.path().filename().string();
+			vec.push_back(file);
+		}
+	}
+	return vec;
+}
+
 void print_vec(std::vector<int> &arr){
 	for (size_t i = 0; i < arr.size(); i++){
 		std::cout << "loc: " << i << ' ' << arr[i] << std::endl;
 	}
 }
 
-void read(std::vector<int> &arr, int index){//reference to vector
+int get(std::vector<int> &arr, int index){
+	try{
+		return arr.at(index);
+	}catch(std::out_of_range& e){
+		std::cout << e.what() << std::endl;
+		return 0;
+	}
+}
+//end of helpers
+
+void read(std::vector<int> &arr, int index){
 	try{
 		std::string input;
 		std::cout << "Input the four digit word you want, if data insert opcode 00 then the number you wish thats two digits so if i wanted 69, 0069, if negative 69, -0069: " << std::endl;
@@ -49,8 +74,45 @@ void read(std::vector<int> &arr, int index){//reference to vector
 	}
 }
 
-int main() {
-	std::ifstream file("Test1.txt");
+void write(std::vector<int> &arr, int index){//idk why its called write, it writes to screen, should be called print...
+	std::cout << get(arr, index) << std::endl;
+}
+
+void load(std::vector<int> &arr, int index, int &accumulator){
+	accumulator = get(arr, index);
+}
+
+void store(std::vector<int> &arr, int index, const int accumulator){
+	//for error checking here, i WAS thinking catch if accumulator is not a int but it will throw error on funciton call and never hit my try if its not an int 
+	try{
+		arr.at(index) = accumulator;
+	}catch(std::out_of_range& e){//i just realized im not using the e, like e.what() to print the error message but my messages say what it wouldav, basically
+		std::cout << "Memory has capacity 100 spots, so last addressable place is 99" << std::endl;
+	}
+}
+
+void add(std::vector<int> &arr, int index, int &accumulator){//updates the accumulator by reference of course
+	int word = get(arr, index);
+	accumulator = accumulator + word;
+}
+
+void subtract(std::vector<int> &arr, int index, int &accumulator){
+	accumulator = accumulator - get(arr, index);
+}
+
+
+int main() {//print out all .txt files in directory, take input on which one and throw error if name is not an existing .txt file
+	//select .txt BasicML program part
+	std::cout << "Input the exact file name you want, if main.txt, type exactly that: " << std::endl;
+	std::vector<std::string> files = get_all_txt();
+
+	for (const auto& i : files){
+		std::cout << i << std::endl;
+	}
+
+	std::string file_choice;
+	std::cin >> file_choice;
+	std::ifstream file(file_choice);
 
 	std::vector<int> memory;/*initializes all 100 elements to zeros if vector(100)... gotta reserve space not vector(100)*/
 	memory.reserve(100);
@@ -61,10 +123,26 @@ int main() {
 	}
 	//while loop for executing all instructions, keep looping while array holds stuff
 	read(memory, 0);
-	print_vec(memory);
+	write(memory, 0);
+	int accumulator = 31;
+	load(memory, 0, accumulator);
+	std::cout << accumulator << std::endl;
+	store(memory, 0, accumulator);
+	std::cout << memory.at(0) << std::endl;
+	add(memory, 0, accumulator);
+	std::cout << accumulator << std::endl;
+	subtract(memory, 0, accumulator);
+	std::cout << accumulator << std::endl;
+	
+	//print_vec(memory);
+
 	/*
 	while (memory.size() > 0){
 		
+		instruction = memory[i]
+
+		opcode = memory[i] / 100
+		operand = memory[i] % 100; ex 2006 % 100 == 6
 
 		//last thing of course, remove instruction at index we want
 		memory.erase(memory.begin() + 2);//element at index 2 is removed
