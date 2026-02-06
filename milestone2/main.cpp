48354357518980516ef0a12a5de108805482c7fb
Mr-Taylor-French <jacen.taylor12@gmail.com>
2026-02-06 11:44:06 -0700

    started first branch function

--- milestone2/main.cpp
+++ milestone2/main.cpp
@@ -3,27 +3,6 @@
 #include <string>
 #include <vector>
 #include <filesystem>
-/*
-
-READ = 10 Read a word from the keyboard into a specific location in memory.
-WRITE = 11 Write a word from a specific location in memory to screen.
-
-Load/store operations:
-LOAD = 20 Load a word from a specific location in memory into the accumulator.
-STORE = 21 Store a word from the accumulator into a specific location in memory.
-
-Arithmetic operation:
-ADD = 30 Add a word from a specific location in memory to the word in the accumulator (leave the result in the accumulator)
-SUBTRACT = 31 Subtract a word from a specific location in memory from the word in the accumulator (leave the result in the accumulator)
-DIVIDE = 32 Divide the word in the accumulator by a word from a specific location in memory (leave the result in the accumulator).
-MULTIPLY = 33 multiply a word from a specific location in memory to the word in the accumulator (leave the result in the accumulator).
-
-Control operation:
-BRANCH = 40 Branch to a specific location in memory
-BRANCHNEG = 41 Branch to a specific location in memory if the accumulator is negative.
-BRANCHZERO = 42 Branch to a specific location in memory if the accumulator is zero.
-HALT = 43 Pause the program
- */
 
 
 //helper function section
@@ -86,7 +65,7 @@ void store(std::vector<int> &arr, int index, const int accumulator){
 	//for error checking here, i WAS thinking catch if accumulator is not a int but it will throw error on funciton call and never hit my try if its not an int 
 	try{
 		arr.at(index) = accumulator;
-	}catch(std::out_of_range& e){//i just realized im not using the e, like e.what() to print the error message but my messages say what it wouldav, basically
+	}catch(std::out_of_range &e){//i just realized im not using the e, like e.what() to print the error message but my messages say what it wouldav, basically
 		std::cout << "Memory has capacity 100 spots, so last addressable place is 99" << std::endl;
 	}
 }
@@ -99,6 +78,27 @@ void add(std::vector<int> &arr, int index, int &accumulator){//updates the accum
 void subtract(std::vector<int> &arr, int index, int &accumulator){
 	accumulator = accumulator - get(arr, index);
 }
+
+void multiply(std::vector<int> &arr, int index, int &accumulator){
+	accumulator = accumulator * get(arr, index);
+}
+
+void divide(std::vector<int> &arr, int index, int &accumulator){
+	try{
+		int word = get(arr, index);
+		if (word == 0){
+			throw std::logic_error("");
+		}
+		accumulator = accumulator / word;//if it is not an integer upon division, i think its truncated into one.
+	}catch(std::logic_error &e){
+		std::cout << "Cant divide by zero bro, maths gonna yell at you for that one and im inclined to agree" << std::endl;
+	}
+}
+
+void branch(int index, int &pc){//on all branch guys, make sure pc is not updated, maybe just pc-- in each but thats unclear
+	pc = index;
+}
+
 
 
 int main() {//print out all .txt files in directory, take input on which one and throw error if name is not an existing .txt file
@@ -121,7 +121,7 @@ int main() {//print out all .txt files in directory, take input on which one and
 	while (file >> file_input) { //could do a for loop then have it check if the statement was true per iter. but that seems more inefficient than this
 		memory.push_back(file_input);//inserts values at i then increments after,
 	}
-	//while loop for executing all instructions, keep looping while array holds stuff
+
 	read(memory, 0);
 	write(memory, 0);
 	int accumulator = 31;
@@ -136,17 +136,19 @@ int main() {//print out all .txt files in directory, take input on which one and
 	
 	//print_vec(memory);
 
-	/*
-	while (memory.size() > 0){
-		
-		instruction = memory[i]
+	int pc = 0;
+	int instruction, opcode, operand;
+	while (memory.size() > 0){ //while loop for executing all instructions, keep looping while array holds stuff
 
-		opcode = memory[i] / 100
-		operand = memory[i] % 100; ex 2006 % 100 == 6
+		instruction = memory.at(pc);
+
+		opcode = memory.at(pc) / 100;
+		operand = memory.at(pc) % 100; //ex 2006 % 100 == 6
+
 
 		//last thing of course, remove instruction at index we want
 		memory.erase(memory.begin() + 2);//element at index 2 is removed
 	}
-	*/
+	
 	return 0;
 }
