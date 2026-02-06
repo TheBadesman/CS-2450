3c18ee819dc62e5ba821547bf3979e474d5325ce
Mr-Taylor-French <jacen.taylor12@gmail.com>
2026-02-05 18:17:14 -0700

    got read working on my version

--- milestone2/main.cpp
+++ milestone2/main.cpp
@@ -1,18 +1,62 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-using namespace std;
+#include <vector>
+
+/*
+
+READ = 10 Read a word from the keyboard into a specific location in memory.
+WRITE = 11 Write a word from a specific location in memory to screen.
+
+Load/store operations:
+LOAD = 20 Load a word from a specific location in memory into the accumulator.
+STORE = 21 Store a word from the accumulator into a specific location in memory.
+
+Arithmetic operation:
+ADD = 30 Add a word from a specific location in memory to the word in the accumulator (leave the result in the accumulator)
+SUBTRACT = 31 Subtract a word from a specific location in memory from the word in the accumulator (leave the result in the accumulator)
+DIVIDE = 32 Divide the word in the accumulator by a word from a specific location in memory (leave the result in the accumulator).
+MULTIPLY = 33 multiply a word from a specific location in memory to the word in the accumulator (leave the result in the accumulator).
+
+Control operation:
+BRANCH = 40 Branch to a specific location in memory
+BRANCHNEG = 41 Branch to a specific location in memory if the accumulator is negative.
+BRANCHZERO = 42 Branch to a specific location in memory if the accumulator is zero.
+HALT = 43 Pause the program
+ */
+void print_vec(std::vector<int> &arr){
+	for (size_t i = 0; i < arr.size(); i++){
+		std::cout << "loc: " << i << ' ' << arr[i] << std::endl;
+	}
+}
+
+void read(std::vector<int> &arr, int index){//reference to vector
+	std::string input;
+	std::cout << "Input the four digit word you want, if data insert opcode 00 then the number you wish thats two digits so if i wanted 69, 0069, if negative 69, -0069: " << std::endl;
+	std::cin >> input;
+	arr[index] = std::stoi(input);
+}
+
 int main() {
-	ifstream file("Test1.txt");
+	std::ifstream file("Test1.txt");
 
-	int memory[100];
-	int input_val;
-	int i = 0; 
-	while (file >> input_val) { //could do a for loop then have it check if the statement was true per iter. but that seems more inefficient than this
-		memory[i++] = input_val;//inserts values at i then increments after,
-		cout << memory[--i] << endl;
-	}//next first steps, read top instruction, ex: 1007, so read(10) input into location 07, 
+	std::vector<int> memory;/*initializes all 100 elements to zeros if vector(100)... gotta reserve space not vector(100)*/
+	memory.reserve(100);
 
-	
+	int file_input;
+	while (file >> file_input) { //could do a for loop then have it check if the statement was true per iter. but that seems more inefficient than this
+		memory.push_back(file_input);//inserts values at i then increments after,
+	}
+	//while loop for executing all instructions, keep looping while array holds stuff
+	read(memory, 0);
+	print_vec(memory);
+	/*
+	while (memory.size() > 0){
+		
+
+		//last thing of course, remove instruction at index we want
+		memory.erase(memory.begin() + 2);//element at index 2 is removed
+	}
+	*/
 	return 0;
 }
