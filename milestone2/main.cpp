c4e8c4e504c91af7e2e2cac2ef0cafbfadf4dfd8
Mr-Taylor-French <jacen.taylor12@gmail.com>
2026-02-06 21:47:08 -0700

    My version of the program is now done, there might be more error checks I think of or modifications but it works on failed f.txt and passed w.txt tests

--- milestone2/main.cpp
+++ milestone2/main.cpp
@@ -3,7 +3,7 @@
 #include <string>
 #include <vector>
 #include <filesystem>
-
+#include <cstdlib>
 
 //helper function section
 std::vector<std::string> get_all_txt(){
@@ -29,6 +29,7 @@ int get(std::vector<int> &arr, int index){
 		return arr.at(index);
 	}catch(std::out_of_range& e){
 		std::cout << e.what() << std::endl;
+		std::exit(1);
 		return 0;
 	}
 }
@@ -37,19 +38,29 @@ int get(std::vector<int> &arr, int index){
 void read(std::vector<int> &arr, int index){
 	try{
 		std::string input;
-		std::cout << "Input the four digit word you want, if data insert opcode 00 then the number you wish thats two digits so if i wanted 69, 0069, if negative 69, -0069: " << std::endl;
+		std::cout << "Input the sign + or - then the four digit word you want, if data insert opcode 00 then the number you wish thats two digits so if i wanted 69, +0069, if negative 69, -0069: " << std::endl << std::flush;
 		std::cin >> input;
-		if (input.length() > 5 || input.length() < 4){//length can only be 4 or 5 characters long
+		char sign[2] = {'-', '+'};
+		if (input.length() != 5){//length can only be 5 characters long
 			throw std::out_of_range("");
+		}else if (input[0] != sign[0] && input[0] != sign[1]){
+			throw std::invalid_argument("Instruction read does not hold a sign, it needs to be a signed number dog");
+		}for (size_t i = 1; i < input.length(); i++){
+			if (std::isdigit(input[i]) == false){
+				throw std::invalid_argument("Instruction read is not a number");
+			}
 		}
 		arr[index] = std::stoi(input);
 	}catch(const std::invalid_argument& e){//reference so you dont copy the exception object, didnt know that helped perforance but good shit ig
 		std::cout << "You didnt enter a number at all..." << std::endl;
+		std::exit(1);
 	}catch(const std::out_of_range& e){
 		std::cout << "You inputed a number greater than length five(number 4 digits but 5 for potential sign ie -0099)"  << std::endl;
+		std::exit(1);
 	}
 	catch(...){
 		std::cout << "Unknown issue :(" << std::endl;
+		std::exit(1);
 	}
 }
 
@@ -67,6 +78,7 @@ void store(std::vector<int> &arr, int index, const int accumulator){
 		arr.at(index) = accumulator;
 	}catch(std::out_of_range &e){//i just realized im not using the e, like e.what() to print the error message but my messages say what it wouldav, basically
 		std::cout << "Memory has capacity 100 spots, so last addressable place is 99" << std::endl;
+		std::exit(1);
 	}
 }
 
@@ -92,6 +104,7 @@ void divide(std::vector<int> &arr, int index, int &accumulator){
 		accumulator = accumulator / word;//if it is not an integer upon division, i think its truncated into one.
 	}catch(std::logic_error &e){
 		std::cout << "Cant divide by zero bro, maths gonna yell at you for that one and im inclined to agree" << std::endl;
+		std::exit(1);
 	}
 }
 
@@ -99,7 +112,26 @@ void branch(int index, int &pc){//on all branch guys, make sure pc is not update
 	pc = index;
 }
 
+void branchNeg(int index, int &pc, int &accumulator){
+	if (accumulator < 0){
+        	pc = index;
+	}else{
+		pc++;//as if it doesnt branch, pc needs to be incremented and it aint happenin in the switch of course.
+	}
+}
 
+void branchZero(int index, int &pc, int &accumulator){//on all branch guys, make sure pc is not updated, maybe just pc-- in each but thats unclear
+	if (accumulator == 0){
+        	pc = index;
+	}else{
+		pc++;
+	}
+}
+
+void halt(int pc){
+	std::cout << "opcode 43 read, Program Halt at PC: " << pc << " ,Goodbye." << std::endl;
+	std::exit(0);
+}
 
 int main() {//print out all .txt files in directory, take input on which one and throw error if name is not an existing .txt file
 	//select .txt BasicML program part
@@ -114,40 +146,105 @@ int main() {//print out all .txt files in directory, take input on which one and
 	std::cin >> file_choice;
 	std::ifstream file(file_choice);
 
-	std::vector<int> memory;/*initializes all 100 elements to zeros if vector(100)... gotta reserve space not vector(100)*/
-	memory.reserve(100);
+	std::vector<int> memory(100, 0);
 
-	int file_input;
+	std::string file_input;
+	int int_file;
+	char sign[2] = {'-', '+'};
+	int load_index = 0;
+	if (!file.is_open()){
+		std::cout << "File aint found dog, ig we fuck off then" << std::endl;
+		return -1;
+	}
 	while (file >> file_input) { //could do a for loop then have it check if the statement was true per iter. but that seems more inefficient than this
-		memory.push_back(file_input);//inserts values at i then increments after,
+		try{
+			if (file_input.length() != 5){//length can only be 5 characters long
+				throw std::out_of_range("");
+			}else if (file_input[0] != sign[0] && file_input[0] != sign[1]){
+				throw std::invalid_argument("Instruction read does not hold a sign, it needs to be a signed number dog");
+			}for (size_t i = 1; i < file_input.length(); i++){
+				if (std::isdigit(file_input[i]) == false){
+					throw std::invalid_argument("Instruction read is not a number");
+				}
+			}
+			int_file = std::stoi(file_input);
+			memory[load_index++] = int_file;
+		}catch(const std::invalid_argument& e){
+			std::cout << e.what() << std::endl;
+			std::exit(1);
+		}catch(const std::out_of_range& e){
+			std::cout << "File contains instruction not length five(5 for potential sign ie -0099), bad instruction: " << file_input << std::endl;
+			std::exit(1);
+		}
+		catch(...){
+			std::cout << "Unknown issue :(" << std::endl;
+			std::exit(1);
+		}
+	}
+	for (size_t i = load_index; i < 100; i++){//fill rest of memory with halt instruction so if it ever jumps to a weird place it shouldnt be, halt
+		memory.at(i) = 4300;
 	}
 
-	read(memory, 0);
-	write(memory, 0);
-	int accumulator = 31;
-	load(memory, 0, accumulator);
-	std::cout << accumulator << std::endl;
-	store(memory, 0, accumulator);
-	std::cout << memory.at(0) << std::endl;
-	add(memory, 0, accumulator);
-	std::cout << accumulator << std::endl;
-	subtract(memory, 0, accumulator);
-	std::cout << accumulator << std::endl;
-	
-	//print_vec(memory);
-
 	int pc = 0;
+	int accumulator = 0;
 	int instruction, opcode, operand;
-	while (memory.size() > 0){ //while loop for executing all instructions, keep looping while array holds stuff
+	bool executing = true;
+	while (executing){ //while loop for executing all instructions, keep looping while array holds stuff
+		if (pc < 0 || pc >= 100) {
+			std::cout << "PC out of bounds: " << pc << std::endl;
+			exit(1);
+		}
 
-		instruction = memory.at(pc);
+		instruction = get(memory, pc);
+		instruction = std::abs(instruction);
 
-		opcode = memory.at(pc) / 100;
-		operand = memory.at(pc) % 100; //ex 2006 % 100 == 6
+		opcode = instruction / 100;
+		operand = instruction % 100; //ex 2006 % 100 == 6
+		
+		switch (opcode) {
+			case 10:
+				read(memory, operand); 
+				break;
+			case 11: 
+				write(memory, operand); 
+				break;
+			case 20:
+				load(memory, operand, accumulator); 
+				break;
+			case 21: 
+				store(memory, operand, accumulator); 
+				break;
+			case 30: 
+				add(memory, operand, accumulator);
+				break;
+			case 31: 
+				subtract(memory, operand, accumulator);
+				break;
+			case 32: 
+				divide(memory, operand, accumulator);
+				break;
+			case 33: 
+				multiply(memory, operand, accumulator);
+				break;
+			case 40: 
+				branch(operand, pc); 
+				continue;//jumps to next iteration of loop after branch command happens.
+			case 41: 
+				branchNeg(operand, pc, accumulator);
+				continue;
+			case 42: 
+				branchZero(operand, pc, accumulator);
+				continue;
+			case 43: 
+				halt(pc);
+				executing = false;//just in case something weird happens on the exit in halt, break the while here
+				break;
 
-
-		//last thing of course, remove instruction at index we want
-		memory.erase(memory.begin() + 2);//element at index 2 is removed
+			default:
+				std::cout << "Invalid opcode: " << opcode << ". PC location: " << pc << std::endl;
+				std::exit(1);
+		}
+		pc++;	
 	}
 	
 	return 0;
