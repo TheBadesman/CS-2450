9a8ec84bddcfadf958bc7b29edbd0be6774cec9a
TheBadesman <Cameron@ottleys.net>
2026-02-07 00:02:48 -0700

    Adding functions
    Adds the updated READ and WRITE functions, as well as the new Branching functions

--- Milestone 2/main.cpp
+++ Milestone 2/main.cpp
@@ -14,15 +14,49 @@ Created: 01/28/2026
 //using name space std to make coding easier
 using namespace std;
 
-//Reads a word from the keyboard into a specific location in memory
-void READ(){
-    
-};
+int get(string memory[100], int memory_address){//helper with error checking so you dont have to do that try catch on every get of the memory array or vector.
+        try{
+                return memory[100].at(memory_address);
+        }catch(out_of_range& e){
+                cout << e.what() << std::endl;
+                exit(1);
+                return 0;
+        }
+}
 
-//Writes a word from a specific location in memory to screen
-void WRITE(){
-    
-};
+
+
+void READ(string memory[100], int memory_address){
+        try{
+                string input;
+                cout << "Input the sign + or - then the four digit word you want, if data insert opcode 00 then the number you wish thats two digits so if i wanted 69, +0069, if negative 69, -0069: " << std::endl;
+                cin >> input;
+                if (input.length() != 5){//length can only be 5 characters long
+                        throw out_of_range("Your input is either too short or too long!");
+                }else if (input[0] != '-' && input[0] != '+'){
+                        throw invalid_argument("Instruction read does not hold a sign, it needs to be a signed number dog");
+                }for (size_t i = 1; i < input.length(); i++){
+                        if (isdigit(input[i]) == false){
+                                throw invalid_argument("Instruction read is not a number");
+                        }
+                }
+                memory[memory_address] = input;
+        }catch(const invalid_argument& e){//reference so you dont copy the exception object
+                cout << "You didnt enter a number at all..." << endl;
+                exit(1);
+        }catch(const out_of_range& e){
+                cout << "You inputed a number greater than length five(number 4 digits but 5 for potential sign ie -0099)"  << std::endl;
+                exit(1);
+        }
+        catch(...){
+                cout << "Unknown issue :(" << endl;
+                exit(1);
+        }
+}
+
+void WRITE(string memory[100], int memory_address){
+        std::cout << get(memory, memory_address) << std::endl;
+}
 
 //Loads a word from a specific location in memory into the accumulator
 void LOAD(){
@@ -55,18 +89,30 @@ void MULTIPLY(){
 };
 
 //Branchs to a specific location in memory
-void BRANCH(){
-    
+int BRANCH(int memory_address){
+    return memory_address - 1;
 };
 
 //Branchs to a specific location in memory if the accumulator is negative.
-void BRANCHNEG(){
-    
+int BRANCHNEG(int accumulator,int old_address, int memory_address){
+    //checks if the accumaltor is less then zero, and branches if it is
+    if (accumulator < 0){
+        return memory_address - 1;
+    }
+
+    //returns the old address if it is not
+    return old_address;
 };
 
 //Branchs to a specific location in memory if the accumulator is zero
-void BRANCHZERO(){
-    
+int BRANCHZERO(int accumulator, int old_address, int memory_address){
+    //checks if the accumaltor is equal to zero, and branches if it is
+    if (accumulator == 0){
+        return memory_address - 1;
+    }
+
+    //returns the old address if it is not
+    return old_address;
 };
 
 //function to open a file and read its contents
@@ -113,17 +159,18 @@ void reader(std::string fileName){
                 throw 505;
             }
 
-            if (command == "10"){READ();}
-            else if (command == "11"){WRITE();}
+            //if else block to run the commands
+            if (command == "10"){READ(memory, stoi(memory[address].substr(3,2)));}
+            else if (command == "11"){WRITE(memory, stoi(memory[address].substr(3,2)));}
             else if (command == "20"){LOAD();}
             else if (command == "21"){STORE();}
             else if (command == "30"){ADD();}
             else if (command == "31"){SUBTRACT();}
             else if (command == "32"){DIVIDE();}
             else if (command == "33"){MULTIPLY();}
-            else if (command == "40"){BRANCH();}
-            else if (command == "41"){BRANCHNEG();}
-            else if (command == "42"){BRANCHZERO();}
+            else if (command == "40"){address = BRANCH(stoi(memory[address].substr(3,2)));}
+            else if (command == "41"){BRANCHNEG(address, stoi(memory[address].substr(3,2)));}
+            else if (command == "42"){BRANCHZERO(address, stoi(memory[address].substr(3,2)));}
             else if (command == "43"){
                 //breaks the while true, leaving the loop if halt (command 43) is called
                 break;
