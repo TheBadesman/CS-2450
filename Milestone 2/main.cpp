753b7f1d828dcf5e524155420a57a59128e5d4ba
Mr-Taylor-French <jacen.taylor12@gmail.com>
2026-02-06 20:05:19 -0700

    Update main.cpp
    Put in my helper function and the read/write functions

--- Milestone 2/main.cpp
+++ Milestone 2/main.cpp
@@ -9,20 +9,55 @@ Created: 01/28/2026
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <iostream>
 
 //using name space std to make coding easier
 using namespace std;
 
-//Reads a word from the keyboard into a specific location in memory
-void READ(){
-    
-};
 
-//Writes a word from a specific location in memory to screen
-void WRITE(){
-    
-};
+int get(std::vector<int> &arr, int index){//helper with error checking so you dont have to do that try catch shit on every get of the memory array or vector, whatever.
+        try{
+                return arr.at(index);
+        }catch(std::out_of_range& e){
+                std::cout << e.what() << std::endl;
+                std::exit(1);
+                return 0;
+        }
+}
+
+
+
+void read(std::vector<int> &arr, int index){
+        try{
+                std::string input;
+                std::cout << "Input the sign + or - then the four digit word you want, if data insert opcode 00 then the number you wish thats two digits so if i wanted 69, +0069, if negative 69, -0069: " << std::endl;
+                std::cin >> input;
+                char sign[2] = {'-', '+'};
+                if (input.length() != 5){//length can only be 5 characters long
+                        throw std::out_of_range("");
+                }else if (input[0] != sign[0] && input[0] != sign[1]){
+                        throw std::invalid_argument("Instruction read does not hold a sign, it needs to be a signed number dog");
+                }for (size_t i = 1; i < input.length(); i++){
+                        if (std::isdigit(input[i]) == false){
+                                throw std::invalid_argument("Instruction read is not a number");
+                        }
+                }
+                arr[index] = std::stoi(input);
+        }catch(const std::invalid_argument& e){//reference so you dont copy the exception object, didnt know that helped perforance but good shit ig
+                std::cout << "You didnt enter a number at all..." << std::endl;
+                std::exit(1);
+        }catch(const std::out_of_range& e){
+                std::cout << "You inputed a number greater than length five(number 4 digits but 5 for potential sign ie -0099)"  << std::endl;
+                std::exit(1);
+        }
+        catch(...){
+                std::cout << "Unknown issue :(" << std::endl;
+                std::exit(1);
+        }
+}
+
+void write(std::vector<int> &arr, int index){//idk why its called write, it writes to screen, should be called print...
+        std::cout << get(arr, index) << std::endl;
+}
 
 //Loads a word from a specific location in memory into the accumulator
 void LOAD(){
